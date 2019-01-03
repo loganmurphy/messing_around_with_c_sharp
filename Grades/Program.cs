76709a3f32e2e9cf676fe198d51ed603ff394ea2
loganmurphy <loganmurphy1984@gmail.com>
2019-01-02 21:59:14 -0600

    playing with events and delegates

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -9,8 +9,7 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook();
-            book.NameChanged += new NameChangedDelegate(OnNameChanged);
-            book.NameChanged += new NameChangedDelegate(OnNameChanged2);
+            book.NameChanged += OnNameChanged;
 
             book.Name = "My grade book";
             book.Name = "Another new grade book";
@@ -27,14 +26,9 @@ namespace Grades
             WriteResult("Lowest", stats.LowestGrade);
         }
 
-        static void OnNameChanged(string existingName, string newName)
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
         {
-            Console.WriteLine($"Grade book changing name from {existingName} to {newName}");
-        }
-
-        static void OnNameChanged2(string existingName, string newName)
-        {
-            Console.WriteLine("***");
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
         }
 
         static void WriteResult(string description, int result)
--- Grades/Properties/GradeBook.cs
+++ Grades/Properties/GradeBook.cs
@@ -44,14 +44,17 @@ namespace Grades
                 {
                     if(_name != value)
                     {
-                        NameChanged(_name, value);
+                        NameChangedEventArgs args = new NameChangedEventArgs();
+                        args.ExistingName = _name;
+                        args.NewName = value;     
+                        NameChanged(this, args);
                     }
                     _name = value;
                 }
             }
         }
 
-        public NameChangedDelegate NameChanged;
+        public event NameChangedDelegate NameChanged;
         public List<float> grades;
         private string _name;
     }
