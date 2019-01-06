21372c52e99e43c8821d56cace188e112327cf0a
loganmurphy <loganmurphy1984@gmail.com>
2019-01-05 21:34:17 -0600

    familiarizing myself with switch/if else statments in c#

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -7,38 +7,27 @@ namespace Grades
         public static void Main(string[] args)
 
         {
-
             GradeBook book = new GradeBook();
-            book.NameChanged += OnNameChanged;
-
-            book.Name = "My grade book";
-            book.Name = "Another new grade book";
 
             book.AddGrade(100);
             book.AddGrade(95);
             book.AddGrade(60);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(book.Name);
 
             WriteResult("Average", stats.AverageGrade);
-            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Highest", stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
-        }
-
-        static void OnNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
-        }
-
-        static void WriteResult(string description, int result)
-        {
-            Console.WriteLine($"{description}: {result:F2}");
+            WriteResult(stats.Description, stats.LetterGrade);
         }
 
         static void WriteResult(string description, float result)
         {
             Console.WriteLine($"{description}: {result:F2}");
+        }
+        static void WriteResult(string description, string result)
+        {
+            Console.WriteLine($"{description}: {result}");
         }
     }
 }
